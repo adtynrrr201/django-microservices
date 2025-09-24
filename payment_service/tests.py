6d15de0527423d51a6584148e5189dc667c0b0b9
adtynrrr201 <nuraditya403@gmail.com>
2025-09-24 15:34:03 +0700

    update terbaru

--- payment_service/tests.py
+++ payment_service/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 class AuthHealthTest(TestCase):
-    def test_auth_health(self):
+    def test_payment_health(self):
         response = self.client.get("/payment/health/")
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(
