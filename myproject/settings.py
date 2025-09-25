1ef3b50dc8a6ce2e0e69464761100e2ccac371e1
adtynrrr201 <nuraditya403@gmail.com>
2025-09-25 17:51:11 +0700

    update endpoint

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'auth_service',
     'order_service',
-    'payment_service',
 ]
 
 MIDDLEWARE = [
--- payment_service/tests.py
+++ payment_service/tests.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from django.urls import reverse
 
 class AuthHealthTest(TestCase):
-    def test_payment_health(self):
-        response = self.client.get("/payment/health/")
+    def test_auth_health(self):
+        response = self.client.get("/auth/health/")
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(
             response.content,
-            {"status": "ok", "service": "payment-service"}
+            {"status": "ok", "service": "auth-service"}
         )
