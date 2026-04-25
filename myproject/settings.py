c6576421aee6e4e7dc0e9c37541281ae1ea2372a
adtynrrr201 <nuraditya403@gmail.com>
2026-04-26 05:46:23 +0700

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB', 'coba'),
-        'USER': os.environ.get('POSTGRES_USER', 'adityanur'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '201201'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
